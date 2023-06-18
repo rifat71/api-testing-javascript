1d017c7cdf6fa009be9e9892644f371fe4f1c3b6
rifat71 <rifatmahmud24311@gmail.com>
2023-06-19 01:55:35 +0600

    add script

--- test/test1.spec.js
+++ test/test1.spec.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const expect = require('chai').expect;
 const app = express();
 
-app.get('/first', (err, res){
+app.get('/first', (err, res) => {
     res.status(200).json({"ok": "responce"});
 });
 
